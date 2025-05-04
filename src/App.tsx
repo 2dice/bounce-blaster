7d8cf7d0b5f78b94806090a479b8693471aa9688
2dice <rjhxx419@gmail.com>
2025-05-04 19:17:17 +0900

    fix(App): 正しいコードに修正してlint-stagedテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import './index.css'
+import './index.css';
 
 function App() {
-  // わざとセミコロンを抜いたり、不要なconsole.logを入れたりする
-  console.log("This should cause a lint error")
-  
-  const unused_variable = "This should cause another lint error"
-  
   return (
-    <div className="text-white bg-gray-900 flex-col min-h-screen w-full items-center justify-center flex">
-      <h1 className="font-bold text-4xl mb-8 text-center">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="mb-8 text-center text-4xl font-bold">
         Hello Bounce Blaster
       </h1>
       <canvas
         id="game-canvas"
-        className="border h-[720px] w-[960px] bg-zinc-800"
+        className="h-[720px] w-[960px] border bg-zinc-800"
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
